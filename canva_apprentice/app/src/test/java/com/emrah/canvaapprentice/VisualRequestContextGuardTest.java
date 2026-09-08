d4cea0c10d9d3b072f85fe9dec7d9bfc15b6ba54
lolo9723 <inaltekinemrah48@gmail.com>
2026-09-08 05:41:22 +0300

    Test screenshot and UI-tree context binding

---
+++ canva_apprentice/app/src/test/java/com/emrah/canvaapprentice/VisualRequestContextGuardTest.java
@@ -0,0 +1,63 @@
+package com.emrah.canvaapprentice;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public final class VisualRequestContextGuardTest {
+    @Test public void acceptsExactSameUnboundCanvaState() {
+        assertTrue(VisualRequestContextGuard.matches(
+                AgentConstants.CANVA_PACKAGE,
+                AgentConstants.CANVA_PACKAGE,
+                "fp-1",
+                "fp-1",
+                "",
+                false,
+                false));
+    }
+
+    @Test public void rejectsScreenshotWhenTreeChangedDuringCapture() {
+        assertFalse(VisualRequestContextGuard.matches(
+                AgentConstants.CANVA_PACKAGE,
+                AgentConstants.CANVA_PACKAGE,
+                "fp-before",
+                "fp-after",
+                "Design A",
+                true,
+                false));
+    }
+
+    @Test public void rejectsBoundDesignWhenAnchorDisappeared() {
+        assertFalse(VisualRequestContextGuard.matches(
+                AgentConstants.CANVA_PACKAGE,
+                AgentConstants.CANVA_PACKAGE,
+                "fp-1",
+                "fp-1",
+                "Design A",
+                false,
+                false));
+    }
+
+    @Test public void rejectsBoundDesignOnCanvaHomeEvenIfTextIsPresent() {
+        assertFalse(VisualRequestContextGuard.matches(
+                AgentConstants.CANVA_PACKAGE,
+                AgentConstants.CANVA_PACKAGE,
+                "fp-1",
+                "fp-1",
+                "Design A",
+                true,
+                true));
+    }
+
+    @Test public void rejectsWrongActivePackage() {
+        assertFalse(VisualRequestContextGuard.matches(
+                AgentConstants.CANVA_PACKAGE,
+                "com.other.app",
+                "fp-1",
+                "fp-1",
+                "",
+                false,
+                false));
+    }
+}
