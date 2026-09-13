bb4a1ee082a80e4a322533455d1ef4f296d8fea4
lolo9723 <inaltekinemrah48@gmail.com>
2026-09-13 06:41:48 +0300

    Use immutable authority for visual replies

--- canva_apprentice/app/src/main/java/com/emrah/canvaapprentice/AgentAccessibilityService.java
+++ canva_apprentice/app/src/main/java/com/emrah/canvaapprentice/AgentAccessibilityService.java
@@ -541,7 +541,7 @@ public final class AgentAccessibilityService extends AccessibilityService {
                         onStaleTeacherRequestDiscarded();
                         return;
                     }
-                    AgentAction visualAction=TeacherProtocol.parse(reply,marker,true);
+                    AgentAction visualAction=TeacherProtocol.parse(reply,visualAuthority);
                     if(!visualExecutionToken.equals(visualAction.executionLeaseToken)){
                         onStaleTeacherRequestDiscarded();
                         return;
