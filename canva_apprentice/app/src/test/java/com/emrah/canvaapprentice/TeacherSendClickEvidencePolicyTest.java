9b25bf642ab813c7fc5060c8726acac4e62ce6f6
lolo9723 <inaltekinemrah48@gmail.com>
2026-09-15 06:40:13 +0300

    Test fail-closed Send identity canonicality

--- canva_apprentice/app/src/test/java/com/emrah/canvaapprentice/TeacherSendClickEvidencePolicyTest.java
+++ canva_apprentice/app/src/test/java/com/emrah/canvaapprentice/TeacherSendClickEvidencePolicyTest.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 public final class TeacherSendClickEvidencePolicyTest {
     @Test public void unchangedCurrentTargetMayDispatch() {
         assertTrue(TeacherSendClickEvidencePolicy.mayDispatch(true, true, true, true, true, "uid:send-1", "uid:send-1"));
+        assertTrue(TeacherSendClickEvidencePolicy.mayDispatch(true, true, true, true, true, "anc:w7|1:A|1:B", "anc:w7|1:A|1:B"));
     }
 
     @Test public void staleOrChangedTargetFailsClosed() {
@@ -17,5 +18,14 @@ public final class TeacherSendClickEvidencePolicyTest {
         assertFalse(TeacherSendClickEvidencePolicy.mayDispatch(true, true, true, true, false, "uid:send-1", "uid:send-1"));
         assertFalse(TeacherSendClickEvidencePolicy.mayDispatch(true, true, true, true, true, "uid:send-1", "uid:send-2"));
         assertFalse(TeacherSendClickEvidencePolicy.mayDispatch(true, true, true, true, true, "", ""));
+    }
+
+    @Test public void malformedOrAmbiguousIdentityFailsClosed() {
+        assertFalse(TeacherSendClickEvidencePolicy.mayDispatch(true, true, true, true, true, " uid:send-1", " uid:send-1"));
+        assertFalse(TeacherSendClickEvidencePolicy.mayDispatch(true, true, true, true, true, "uid:send-1 ", "uid:send-1 "));
+        assertFalse(TeacherSendClickEvidencePolicy.mayDispatch(true, true, true, true, true, "uid:send\u200B-1", "uid:send\u200B-1"));
+        assertFalse(TeacherSendClickEvidencePolicy.mayDispatch(true, true, true, true, true, "uid:send\n-1", "uid:send\n-1"));
+        assertFalse(TeacherSendClickEvidencePolicy.mayDispatch(true, true, true, true, true, "send-1", "send-1"));
+        assertFalse(TeacherSendClickEvidencePolicy.mayDispatch(true, true, true, true, true, "uid:\uD800", "uid:\uD800"));
     }
 }
