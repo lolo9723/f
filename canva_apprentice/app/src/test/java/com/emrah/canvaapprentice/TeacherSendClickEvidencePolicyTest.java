c4ddba2669692f0b6c3e10777981fc847f4adbb0
lolo9723 <inaltekinemrah48@gmail.com>
2026-09-15 07:40:39 +0300

    Test canonical Send ancestry identities

--- canva_apprentice/app/src/test/java/com/emrah/canvaapprentice/TeacherSendClickEvidencePolicyTest.java
+++ canva_apprentice/app/src/test/java/com/emrah/canvaapprentice/TeacherSendClickEvidencePolicyTest.java
@@ -7,7 +7,8 @@ import org.junit.Test;
 public final class TeacherSendClickEvidencePolicyTest {
     @Test public void unchangedCurrentTargetMayDispatch() {
         assertTrue(TeacherSendClickEvidencePolicy.mayDispatch(true, true, true, true, true, "uid:send-1", "uid:send-1"));
-        assertTrue(TeacherSendClickEvidencePolicy.mayDispatch(true, true, true, true, true, "anc:w7|1:A|1:B", "anc:w7|1:A|1:B"));
+        assertTrue(TeacherSendClickEvidencePolicy.mayDispatch(true, true, true, true, true,
+                "anc:w7|1:A|1:B|1:C|1:D", "anc:w7|1:A|1:B|1:C|1:D"));
     }
 
     @Test public void staleOrChangedTargetFailsClosed() {
@@ -25,7 +26,22 @@ public final class TeacherSendClickEvidencePolicyTest {
         assertFalse(TeacherSendClickEvidencePolicy.mayDispatch(true, true, true, true, true, "uid:send-1 ", "uid:send-1 "));
         assertFalse(TeacherSendClickEvidencePolicy.mayDispatch(true, true, true, true, true, "uid:send\u200B-1", "uid:send\u200B-1"));
         assertFalse(TeacherSendClickEvidencePolicy.mayDispatch(true, true, true, true, true, "uid:send\n-1", "uid:send\n-1"));
+        assertFalse(TeacherSendClickEvidencePolicy.mayDispatch(true, true, true, true, true, "uid:", "uid:"));
         assertFalse(TeacherSendClickEvidencePolicy.mayDispatch(true, true, true, true, true, "send-1", "send-1"));
         assertFalse(TeacherSendClickEvidencePolicy.mayDispatch(true, true, true, true, true, "uid:\uD800", "uid:\uD800"));
+    }
+
+    @Test public void malformedAncestryIdentityFailsClosed() {
+        assertFalse(TeacherSendClickEvidencePolicy.mayDispatch(true, true, true, true, true,
+                "anc:w|1:A|1:B|1:C|1:D", "anc:w|1:A|1:B|1:C|1:D"));
+        assertFalse(TeacherSendClickEvidencePolicy.mayDispatch(true, true, true, true, true,
+                "anc:w7|1:A|1:B", "anc:w7|1:A|1:B"));
+        assertFalse(TeacherSendClickEvidencePolicy.mayDispatch(true, true, true, true, true,
+                "anc:w7|2:A|1:B|1:C|1:D", "anc:w7|2:A|1:B|1:C|1:D"));
+        assertFalse(TeacherSendClickEvidencePolicy.mayDispatch(true, true, true, true, true,
+                "anc:w7|1:A|1:B|1:C|1:Djunk", "anc:w7|1:A|1:B|1:C|1:Djunk"));
+        assertFalse(TeacherSendClickEvidencePolicy.mayDispatch(true, true, true, true, true,
+                "anc:w7|999999999999999999999:A|1:B|1:C|1:D",
+                "anc:w7|999999999999999999999:A|1:B|1:C|1:D"));
     }
 }
